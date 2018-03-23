33295dd32c65c82001c397f07e7352b4db387c5a
khaled <khaled@9545b91f-6fb3-6f42-ada0-5f24a0514697>
2018-03-23 13:46:33 +0000

    Adding first files
    git-svn-id: file:///C/Users/khaled/Desktop/SVN_Server/trunk@2 9545b91f-6fb3-6f42-ada0-5f24a0514697

---
+++ app.js
@@ -0,0 +1,12 @@
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var port = process.env.PORT || 1600;
+app.use(bodyParser.json({limit: '5mb'}));
+
+
+//use routes file
+require('./routes/routes')(app);
+app.listen(port);
+console.log('server is up and running...');
